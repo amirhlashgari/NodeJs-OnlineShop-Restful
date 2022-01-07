8a2ee922a9fbf87af632a6bdae135a52f254a8bc
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-07 09:48:02 +0330

    feat: complete user and post model relations

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const postSchema = new mongoose.Schema(
     title: { type: String, required: true },
     imageUrl: { type: String, required: true },
     content: { type: String, required: true },
-    creator: { type: Object, required: true }
+    creator: { type: mongoose.SchemaTypes.ObjectId, ref: 'User', required: true }
   },
   { timestamps: true }
 );
