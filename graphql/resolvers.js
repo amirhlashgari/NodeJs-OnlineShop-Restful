dd1e49efbd2416f15a0494eacb8d87de875d03f3
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-08 06:26:32 +0330

    feat: add login query to graphql schema and resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 module.exports = {
@@ -26,5 +27,21 @@ module.exports = {
         const user = new User({ email: userInput.email, name: userInput.name, password: hashedPassword });
         const createdUser = await user.save();
         return { ...createdUser._doc, _id: createdUser._id.toString() };
+    },
+    login: async function ({ email, password }) {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            const error = new Error('User not found');
+            error.code = 401;
+            throw error;
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            const error = new Error('Password is incorrect');
+            error.code = 401;
+            throw error;
+        }
+        const token = jwt.sign({ userId: user._id.toString(), email: user.email }, 'SecretKeyToSignToken', { expiresIn: '1h' });
+        return { token: token, userId: user._id.toString() };
     }
 };
