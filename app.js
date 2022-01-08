54254f22a76bcb08a9d292b7e9c207dfde27334c
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-08 05:53:38 +0330

    feat: resolve accessing server through browser

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); //because graphql does not accept OPTIONS method(only POST and GET)
+  }
   next();
 });
 
